6aa1d14aa3d6e13156f42168306358847e1ceca1
Aleksandr <alex-moroz99@yandex.ru>
2023-06-25 12:31:39 +0300

    Удалил бинарные файлы

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
