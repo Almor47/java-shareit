40414869adc404988fbc50494c52e85c79560c77
Aleksandr <alex-moroz99@yandex.ru>
2023-07-09 13:37:58 +0300

    Чекстайл

--- src/test/java/ru/practicum/shareit/ItemRequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/ItemRequestServiceImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.pagination.Pagination;
 import ru.practicum.shareit.request.exception.BadRequestItemRequestException;
 import ru.practicum.shareit.request.exception.NotFoundItemRequestException;
